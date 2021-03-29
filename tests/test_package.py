ee264168b14a7bfb6e520a9b72c70ebb7469033b
chris <christophpryer@gmail.com>
2021-03-28 22:47:04 -0400

    pytest ignore reason typo

--- tests/test_package.py
+++ tests/test_package.py
@@ -2,7 +2,7 @@ import pytest
 import pdappend
 
 
-@pytest.mark.skip("vscode laungh args mixing with cli.Args")
+@pytest.mark.skip("vscode launch args mixing with cli.Args")
 def test_cli():
     pdappend.cli.main()
 
