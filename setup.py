9866345e3ea167476d9048929143e45217d03500
chris <christophpryer@gmail.com>
2021-03-28 22:14:13 -0400

    updated gha, README, & setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,24 @@ from setuptools import setup, find_packages
 from pdappend import __version__
 
 
+CLASSIFIERS = """\
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Topic :: Software Development
+Operating System :: Windows
+Operating System :: Unix
+Operating System :: MacOS
+
+"""
+
+
 long_description = ""
 with open("./README.md") as f:
     long_description = f.read()
@@ -17,11 +35,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cnpls/pdappend",
-    author="Chris",
+    author="Chris Pryer",
     author_email="andromia.software@gmail.com",
     license="MIT",
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=install_requires,
     entry_points={
         "console_scripts": [
@@ -29,5 +47,5 @@ setup(
             "pdappend-gui = pdappend.gui:main",
         ]
     },
-    zip_safe=False,
+    classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
 )
